d5d9f2f66e0868f67f6ac0ce7d50a5c7bacceeb3
Aliisa <aliisarokala@protonmail.com>
2024-09-07 12:48:21 +0300

    update

--- src/main/java/com/example/stringmanipulatorexercise/StringManipulatorAliisa.java
+++ src/main/java/com/example/stringmanipulatorexercise/StringManipulatorAliisa.java
@@ -1,2 +1,16 @@
-package com.example.stringmanipulatorexercise;public class StringManipulatorAliisa {
+package com.example.stringmanipulatorexercise;
+public class StringManipulatorAliisa {
+    String reversed = "";
+    String converted = "";
+    public String reverse(String input) {
+       for (int i = 0; i < input.length(); i++) {
+           reversed = input.charAt(i) + reversed;
+       }
+       return reversed;
+    }
+
+    public String capitalize(String input) {
+        String converted = input.toUpperCase();
+        return converted;
+    }
 }
--- src/test/java/StringManipulatorAliisaTest.java
+++ src/test/java/StringManipulatorAliisaTest.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class StringManipulatorAliisaTest {
+import com.example.stringmanipulatorexercise.StringManipulatorAliisa;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringManipulatorAliisaTest {
+
+    private static StringManipulatorAliisa stringManipulator;
+
+    @BeforeAll
+    public static void setUpAll() {
+        stringManipulator = new StringManipulatorAliisa();
+        System.out.println("String manipulator created\n");
+    }
+
+    @AfterAll
+    public static void tearDownAll() {
+        stringManipulator = null;
+        System.out.println("String manipulator terminated");
+    }
+
+    @BeforeEach
+    public void start() {
+        System.out.println("Starting test");
+    }
+
+    @AfterEach
+    public void finish() {
+        System.out.println("Test finished\n");
+    }
+    @Test
+    public void testReverseString() {
+        System.out.println("Testing reversing string.");
+        assertEquals("tupni", stringManipulator.reverse("input"));
+    }
+
+    @Test
+    public void testCapitalizeString() {
+        System.out.println("Testing capitalizing string.");
+        assertEquals("INPUT", stringManipulator.capitalize("input"));
+    }
 }
